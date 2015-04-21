bdd1698b9fd7a5933e2828b46e78fc8c89547529
Paul Earle <pearle@usgs.gov>
2015-04-21 16:03:43 -0600

    added function to read backbone format, added to readme, fixed a bug in plotcatmap.m that was causing a problem with global maps

--- plotcatmap.m
+++ plotcatmap.m
@@ -7,28 +7,14 @@ maxlat = max(sortcsv(:,2));
 minlat = min(sortcsv(:,2));     
 maxlon = max(sortcsv(:,3));     
 minlon = min(sortcsv(:,3));
+latbuf = 0.1*(maxlat-minlat);
+lonbuf = 0.1*(maxlon-minlon);
 
 %maplonmin = minlon - 5;
-maplonmin = max(minlon-5,-180);
-maplonmax = maxlon + 5;
-maplatmin = minlat - 5;
-maplatmax = maxlat + 5;
-
-% if minlon < -174
-%     maplonmin = -180;
-% end
-
-if maxlon > 174
-    maplonmax = 180;
-end
-
-if minlat > -85 && minlat < 0
-    maplatmin = -90;
-end
-
-if maxlat > 85 && maxlat > 0
-    maplatmax = 90;
-end
+maplonmin = max(minlon-lonbuf,-180);
+maplonmax = min(maxlon+lonbuf,180);
+maplatmin = max(minlat-latbuf,-90);
+maplatmax = min(maxlat+latbuf,90);
 
 % plot quakes
 plot(sortcsv(:,3),sortcsv(:,2),'r.')
@@ -48,5 +34,6 @@ xlabel('Longitude');
 ylabel('Latitude');
 hold on
 
-rectangle('Position',[minlon minlat abs(abs(maxlon)-abs(minlon)) abs(abs(maxlat)-abs(minlat))])
+plot([maplonmin maplonmax maplonmax maplonmin maplonmin],[maplatmax maplatmax maplatmin maplatmin maplatmax],'k');
+%rectangle('Position',[minlon minlat abs(abs(maxlon)-abs(minlon)) abs(abs(maxlat)-abs(minlat))])
 
